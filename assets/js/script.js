5e18a5a5bbe50cce217561decc0cee11d0ba7d3d
Maya Santiago <69778090+msatori@users.noreply.github.com>
2020-09-22 22:37:11 -0500

    added reset value

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,7 +6,14 @@ var taskFormHandler = function(event) {
     event.preventDefault();
     var taskNameInput = document.querySelector("input[name='task-name']").value;
     var taskTypeInput = document.querySelector("select[name='task-type']").value;
-   
+    if (taskNameInput === "" || taskTypeInput === "") {
+        alert("You need to fill out the task form!");
+        return false;
+      }
+
+      //reset the text content
+    formEl.reset();
+
     //package up data as an object
     var taskDataObj = {
         name: taskNameInput,
@@ -15,7 +22,7 @@ var taskFormHandler = function(event) {
 
     //seend it as an argument to createTaskEl
     createTaskEl(taskDataObj);
-}
+};
 
 var createTaskEl = function(taskDataObj) {
     //create list item
